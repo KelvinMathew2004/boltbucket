00854861e818895ea4522e0c756dc2ffafb182ce
Kelvin Mathew <kelvinmathew2004@gmail.com>
2025-10-21 10:01:11 -0500

    Step 5: Update the server

--- server/server.js
+++ server/server.js
@@ -2,16 +2,15 @@ import express from 'express'
 import path from 'path'
 import favicon from 'serve-favicon'
 import dotenv from 'dotenv'
-
-// import the router from your routes file
-
+import cors from 'cors';
+import carRoutes from './routes/cars.js'
 
 dotenv.config()
 
 const PORT = process.env.PORT || 3000
-
 const app = express()
 
+app.use(cors());
 app.use(express.json())
 
 if (process.env.NODE_ENV === 'development') {
@@ -22,15 +21,8 @@ else if (process.env.NODE_ENV === 'production') {
     app.use(express.static('public'))
 }
 
-// specify the api path for the server to use
-
-
-if (process.env.NODE_ENV === 'production') {
-    app.get('/*', (_, res) =>
-        res.sendFile(path.resolve('public', 'index.html'))
-    )
-}
+app.use('/api/cars', carRoutes);
 
 app.listen(PORT, () => {
-    console.log(`server listening on http://localhost:${PORT}`)
-})
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
